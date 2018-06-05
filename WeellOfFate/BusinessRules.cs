ddb369e13dc9a8c48cb356f1678e72a9cbedc106
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-05 21:05:06 +0300

    added the frame for Business Rules class - implementation on pending

---
+++ WeellOfFate/BusinessRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeellOfFate
+{
+    public class BusinessRules//I prefer a separate class because in the exercises it's mentioned that in the future the business rules can suffer changes.
+    {
+        //TO DO: 
+        //TO DO: aici cred ca o sa am logica aia legata de cerinte
+
+        //Un inginer poate face cel mult o schimbare de o jumătate de zi într-o zi.
+        //Un inginer nu poate avea schimbări de jumătate de zi în zile consecutive.
+        //Fiecare inginer ar fi trebuit să aibă o întreagă zi de sprijin în orice perioadă de 2 săptămâni.
+    }
+}
