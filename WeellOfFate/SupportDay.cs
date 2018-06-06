cb6d74cfda9649dd44c9735888667676942aad78
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-06 21:59:09 +0300

    added NullShift in SupportDay enum - this will be the initial state of a shift

--- WeellOfFate/SupportDay.cs
+++ WeellOfFate/SupportDay.cs
@@ -8,6 +8,7 @@ namespace WeellOfFate
 {
     public enum SupportDay// am zis ca e mai bine sa avem un enum, pt ca se presupune ca stiu ca exista 2 sift-uri si nu are rost sa complic situatia cu DateTime
     {
+        NullShift,//this will be the initial stage of a engineers shift
         FirstShift,//repezinta prima parte a zilei, dintr-o zi de munca
         SecondShift//reprezinta a doua parte a zilei, dintr-o zi de munca
     }
