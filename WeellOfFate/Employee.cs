8f22a3837e7ae33e74c4138c990eb15739157a7f
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-06 22:03:14 +0300

    added a new prop for SupportDay enum - Shift{get;set;}

--- WeellOfFate/Employee.cs
+++ WeellOfFate/Employee.cs
@@ -12,11 +12,13 @@ namespace WeellOfFate
         string name;
         string role;
         int id;
+        SupportDay shift;
         #endregion
         #region proprieties
         public string Name { get { return name; } set { name = value; } }
         public string Role { get { return role; } set { role = value; } }
         public int Id { get { return id; } set { id = value; } }
+        public SupportDay Shift { get { return shift; } set { shift = value; } }
         #endregion
         #region constructors
         public Employee()//un constructor gol, pentru situatile in care am nevoie doar sa instantiez un obiect de tip Employee
@@ -34,6 +36,7 @@ namespace WeellOfFate
             Name = N;
             Id = ID;
             Role = ROLE;
+            Shift = SupportDay.NullShift;//the initial state of shift
         }
         #endregion
     }
